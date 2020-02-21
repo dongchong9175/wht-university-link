e04c18d792ab399eca10494f9c4d36414fca4c7d
dongchong9175 <61327117+dongchong9175@users.noreply.github.com>
2020-02-22 01:41:33 +0800

    Create 13101.js

---
+++ 13101.js
@@ -0,0 +1,29 @@
+var univ_list = {
+    school: "南华大学",                        // 学校名称
+    id: "1001",                               // 学校 ID
+    link: [ {
+        name: "南华主页",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://www.usc.edu.cn/"   // 链接地址
+    }, 
+    {
+        name: "研究生管理信息系统",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://61.187.179.68:8099/pyxx/login.aspx"   // 链接地址
+    }, 
+    {
+        name: "教务管理在线",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://61.187.179.66:8924/"   // 链接地址
+    }, 
+     {
+        name: "网络教学平台",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://usc.fanya.chaoxing.com/portal"   // 链接地址
+    }, 
+    {
+        name: "阳光服务平台",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://nhedu.beone2009.com/column/bszn/index.shtml"   // 链接地址
+    }, {
+        ...
+    }, {
+        name: "图书馆",                        // 链接数量小于或等于7个
+        url: "http://tsg.usc.edu.cn/"
+    } ]
+};
